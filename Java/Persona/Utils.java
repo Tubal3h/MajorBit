59ca25198868eb760bdd78beabd5aba3fb3cae85
Macwell <blood_hiphop@hotmail.it>
2025-12-10 00:03:46 +0100

    update studente var array

--- Java/Persona/Utils.java
+++ Java/Persona/Utils.java
@@ -20,27 +20,25 @@ public class Utils {
 	//____
 
     public static ArrayList<Studente> lowGradeStudentes(ArrayList<Studente> arr){
-        // ArrayList<Studente> n = new ArrayList<>();
-
         if(arr == null || arr.size() == 0){
            System.out.println("Zero studenti inseriti");
            return null;
         }else{
-            ArrayList<Studente> n = new ArrayList<Studente>(1);
-            n.add(arr.get(0));
+            ArrayList<Studente> student = new ArrayList<Studente>(1);
+            student.add(arr.get(0));
             if(arr.size() > 1){
                 for(int i=1; i<arr.size(); i++){
-                    if(arr.get(i).getAverage() <= n.get(0).getAverage()){
-                        if(arr.get(i).getAverage() < n.get(0).getAverage()){
-                            n.clear();
-                            n.add(arr.get(i));
+                    if(arr.get(i).getAverage() <= student.get(0).getAverage()){
+                        if(arr.get(i).getAverage() < student.get(0).getAverage()){
+                            student.clear();
+                            student.add(arr.get(i));
                         }else{
-                            n.add(arr.get(i));
+                            student.add(arr.get(i));
                         }
                     }
                 }
             }
-            return n;
+            return student;
         }
     }
 
